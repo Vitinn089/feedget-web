e77458153e70a83e2c3faee85a0cc6a994b6eb9f
Vitor Lopes <vitorlopes089@gmail.com>
2022-06-06 00:07:51 -0300

    Configuração de CORS.

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -2,4 +2,10 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  headers: {                  
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Authorization", 
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
+    "Content-Type": "application/json;charset=UTF-8"                   
+  }
 });

280a93a0579a533056fd1dc0a2180007be86d3b5
Vitor Lopes <vitorlopes089@gmail.com>
2022-06-06 00:19:43 -0300

    Configuração de CORS.

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,12 @@ import { routes } from './routes';
 const app = express();
 
 app.use(cors());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
 app.use(express.json());
 app.use(routes);
 

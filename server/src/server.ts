410a9619544ebaffc144323177f308c21e12cbd2
Vitor Lopes <vitorlopes089@gmail.com>
2022-06-06 00:11:51 -0300

    Configuração de CORS.

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,9 +4,7 @@ import { routes } from './routes';
 
 const app = express();
 
-app.use(cors({
-    origin: 'https://feedget-web-tawny-two.vercel.app',
-}));
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
